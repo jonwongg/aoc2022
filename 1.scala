45e93f4265be72cf0b4c7ebbfa65f23168a08f81
jon <jonathanwong@intricategroup.com>
2022-12-02 13:29:35 -0400

    day 1

--- 1.scala
+++ 1.scala
@@ -1,1 +1,25 @@
-meme
+object Hello {
+    def main(args: Array[String]) = {
+        val elves = Seq(0)
+        var calories = new scala.collection.mutable.ListBuffer[Int]() += 0
+
+        val source = io.Source.fromFile("1.txt")
+        val contents = source.getLines.foreach { b => {
+            if (b == "") calories = calories += 0
+            else calories = calories.init += (calories.last + b.toInt)
+        }}
+        
+        println("max elf calories = " + calories.toList.max)
+
+        val (values, indices) = calories.toList
+          .zipWithIndex       
+          .sortBy(t => -t._1) 
+          .take(3)            
+          .unzip
+
+        println("top 3 elves on the shelves = " + values.sum)
+
+        source.close()
+    }
+
+}
