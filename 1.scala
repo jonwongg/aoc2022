c431d7168e7b2a5c006fae138979226d9022440c
jon <jonathanwong@intricategroup.com>
2022-12-02 13:37:53 -0400

    cleanup

--- 1.scala
+++ 1.scala
@@ -1,25 +1,14 @@
-object Hello {
-    def main(args: Array[String]) = {
-        val elves = Seq(0)
-        var calories = new scala.collection.mutable.ListBuffer[Int]() += 0
+var calories = new scala.collection.mutable.ListBuffer[Int]() += 0
+val source = io.Source.fromFile("1.txt")
+source.getLines.foreach { b => {
+    if (b == "") calories = calories += 0
+    else calories = calories.init += (calories.last + b.toInt)
+}}
 
-        val source = io.Source.fromFile("1.txt")
-        val contents = source.getLines.foreach { b => {
-            if (b == "") calories = calories += 0
-            else calories = calories.init += (calories.last + b.toInt)
-        }}
-        
-        println("max elf calories = " + calories.toList.max)
+println("max elf calories = " + calories.toList.max)
 
-        val (values, indices) = calories.toList
-          .zipWithIndex       
-          .sortBy(t => -t._1) 
-          .take(3)            
-          .unzip
+val (values, indices) = calories.toList.zipWithIndex.sortBy(t => -t._1).take(3).unzip
 
-        println("top 3 elves on the shelves = " + values.sum)
+println("top 3 elves on the shelves = " + values.sum)
 
-        source.close()
-    }
-
-}
+source.close()
