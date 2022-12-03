660952bd42c37bbf809b60261e2644e48bfbc510
jon <jonathanwong@intricategroup.com>
2022-12-03 15:06:22 -0400

    perfection

--- 3-2.scala
+++ 3-2.scala
@@ -1,10 +1,3 @@
 val source = io.Source.fromFile("3.txt")
-val badgerinos = source.getLines.toList.grouped(3).toList
-
-val total = badgerinos.flatMap(_.map(_.toList).reduceLeft(_.intersect(_)).distinct).map { a =>
-  if (a.isUpper) a.toInt - 38 else a.toInt - 96
-} sum
-
-println(total)
-
+println(source.getLines.toList.grouped(3).toList.flatMap(_.map(_.toList).reduceLeft(_.intersect(_)).distinct).map { a => if (a.isUpper) a.toInt - 38 else a.toInt - 96} sum)
 source.close()
