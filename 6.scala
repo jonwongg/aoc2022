142bdc0d2abda40e073b0023823cdc8df14007c0
jon <jonathanwong@intricategroup.com>
2022-12-06 15:30:22 -0400

    day 6 wahoo

--- 6.scala
+++ 6.scala
@@ -2,8 +2,7 @@ val source = io.Source.fromFile("6.txt")
 val lines = source.getLines.mkString.toList
 
 def getConsecUnique(lines: List[Char], chunk: Int) = {
-	lines.sliding(chunk,1).map(_.distinct).zipWithIndex.find(_._1.size == chunk).map(_._2).get + chunk
+	lines.sliding(chunk,1).map(_.distinct).zipWithIndex.find(_._1.size == chunk).map(_._2).map(z => println(z+chunk))
 }
-
-println(getConsecUnique(lines, 4)) // pt1
-println(getConsecUnique(lines, 14))// pt2
+getConsecUnique(lines, 4)  // pt1
+getConsecUnique(lines, 14) // pt2
